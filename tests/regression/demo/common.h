f5f2ada551cfa4f9a5e94c9991de4b18417f00bf
Gagan <socet73@asicfabu.ecn.purdue.edu>
2024-03-24 16:02:16 -0400

    Add missing files in tests folder

--- tests/regression/demo/common.h
+++ tests/regression/demo/common.h
@@ -3,24 +3,16 @@
 
 #define KERNEL_ARG_DEV_MEM_ADDR 0x7ffff000
 
-// typedef struct {
-//   uint32_t num_tasks;
-//   uint32_t task_size;
-//   uint64_t src0_addr;
-//   uint64_t src1_addr;
-//   uint64_t dst_addr;  
-
-// } kernel_arg_t;
+#ifndef TYPE
+#define TYPE float
+#endif
 
 typedef struct {
-  uint32_t num_tasks_nonpriority;
+  uint32_t num_tasks;
   uint32_t task_size;
   uint64_t src0_addr;
   uint64_t src1_addr;
-  uint64_t dst_addr;
-
-  uint32_t num_tasks_priority;
-  //uint32_t priority_task_offset; // Thread number of where the priority tasks start
+  uint64_t dst_addr;  
 } kernel_arg_t;
 
 #endif
--- tests/regression/demo/kernel.cpp
+++ tests/regression/demo/kernel.cpp
@@ -1,69 +1,34 @@
 #include <stdint.h>
 #include <vx_intrinsics.h>
 #include <vx_spawn.h>
-#include <vx_print.h>
 #include "common.h"
-
-// void kernel_body(int task_id, kernel_arg_t* __UNIFORM__ arg) {
-// 	uint32_t count    = arg->task_size;
-// 	int32_t* src0_ptr = (int32_t*)arg->src0_addr;
-// 	int32_t* src1_ptr = (int32_t*)arg->src1_addr;
-// 	int32_t* dst_ptr  = (int32_t*)arg->dst_addr;
-	
-// 	// int32_t dummy_split=0;
-// 	uint32_t offset = task_id * count;
-// 	// if(task_id==0)
-// 	// {
-// 	// 	dummy_split=1;
-// 	// }
-// 	for (uint32_t i = 0; i < count; ++i) {
-// 		dst_ptr[offset+i] = src0_ptr[offset+i] + src1_ptr[offset+i];
-// 	}
-// }
+#include "vx_print.h"
+#include <stdlib.h>
 
 void kernel_body(int task_id, kernel_arg_t* __UNIFORM__ arg) {
-    uint32_t count    = arg->task_size;
-    int32_t* src0_ptr = (int32_t*)arg->src0_addr;
-    int32_t* src1_ptr = (int32_t*)arg->src1_addr;
-    int32_t* dst_ptr  = (int32_t*)arg->dst_addr;
-
+    auto src0_ptr = reinterpret_cast<TYPE*>(arg->src0_addr);
+    auto src1_ptr = reinterpret_cast<TYPE*>(arg->src1_addr);
+    auto dst_ptr = reinterpret_cast<TYPE*>(arg->dst_addr);
+    
+    uint32_t count = arg->task_size;
     uint32_t offset = task_id * count;
-
-    for (uint32_t i = 0; i < count; ++i) {
-        if(src0_ptr[offset+i] % 2 == 0 && src1_ptr[offset+i] % 2 == 0) {
-            dst_ptr[offset+i] = src0_ptr[offset+i] + src1_ptr[offset+i];
-        }
-        else if(src0_ptr[offset+i] % 2 == 1 && src1_ptr[offset+i] % 2 == 0) {
-            dst_ptr[offset+i] = src0_ptr[offset+i] - src1_ptr[offset+i];
-        }
-        else if(src0_ptr[offset+i] % 2 == 0 && src1_ptr[offset+i] % 2 == 1) {
-            dst_ptr[offset+i] = src0_ptr[offset+i] * src1_ptr[offset+i];
-        }
-        else {
-            dst_ptr[offset+i] = 2 * src0_ptr[offset+i] - src1_ptr[offset+i];
-        }
+    int* ptr = (int*) malloc(64);        // malloc here
+    for(int i = 0; i < 64/4; i++){
+        ptr[i] = i;
     }
+    for(int i = 0; i < 32/4; i++){
+      ptr[i] += 3;
+      vx_printf("%d\n", ptr[i]);
+    }
+    free(ptr);
+    for (uint32_t i = 0; i < count; ++i) {
+        dst_ptr[offset+i] = src0_ptr[offset+i] + src1_ptr[offset+i];
+    }
+
 }
-
-// void kernel_body_private(int task_id, kernel_arg_t* __UNIFORM__ arg) {
-// 	uint32_t count    = arg->task_size;
-// 	int32_t* src0_ptr = (int32_t*)arg->src0_addr;
-// 	int32_t* src1_ptr = (int32_t*)arg->src1_addr;
-// 	int32_t* dst_ptr  = (int32_t*)arg->dst_addr;
-	
-// 	uint32_t offset = task_id * count;
-
-// 	for (uint32_t i = 0; i < count; ++i) {
-// 		dst_ptr[offset+i] = src0_ptr[offset+i] + src1_ptr[offset+i];
-// 	}
-// }
 
 int main() {
 	kernel_arg_t* arg = (kernel_arg_t*)KERNEL_ARG_DEV_MEM_ADDR;
-	// kernel_arg_t* arg = (kernel_arg_t*)KERNEL_ARG_DEV_MEM_ADDR2;
-	// vx_printf("Calling VXSpawn\n");
-	vx_spawn_tasks(arg->num_tasks_nonpriority, (vx_spawn_tasks_cb)kernel_body, arg);
-	// vx_printf("Calling VXPSpawn\n");
-	vx_spawn_priority_tasks(arg->num_tasks_priority,arg->num_tasks_nonpriority, (vx_spawn_tasks_cb)kernel_body, arg);
+	vx_spawn_tasks(arg->num_tasks, (vx_spawn_tasks_cb)kernel_body, arg);
 	return 0;
 }
