78ea4f6420c764ab64049767699ccc89144b26f7
gaganng <socet73@asicfabu.ecn.purdue.edu>
2024-04-17 12:26:24 -0400

    scalar core RTL files bifurcated

---
+++ hw/rtl/core/scalar/VX_fetch_scalar.sv
@@ -0,0 +1,274 @@
+// Copyright © 2019-2023
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+`include "VX_define.vh"
+
+module VX_fetch import VX_gpu_pkg::*; #(
+    parameter CORE_ID = 0,
+    parameter THREAD_CNT = `NUM_THREADS
+) (
+    `SCOPE_IO_DECL
+
+    input  wire             clk,
+    input  wire             reset,
+
+    // Icache interface
+    VX_mem_bus_if.master    icache_bus_if,
+    
+    // inputs
+    VX_schedule_if.slave    schedule_if,
+
+    // flush mechanism
+	input [`ISSUE_WIDTH-1:0] branch_mispredict_flush,
+
+    // outputs
+    VX_fetch_if.master      fetch_if
+);
+    `UNUSED_PARAM (CORE_ID)
+    `UNUSED_VAR (reset)
+    localparam ISW_WIDTH  = `LOG2UP(`ISSUE_WIDTH);
+
+    wire icache_req_valid;
+    wire [ICACHE_ADDR_WIDTH-1:0] icache_req_addr;
+    wire [ICACHE_TAG_WIDTH-1:0] icache_req_tag;
+    wire icache_req_ready;
+
+    wire [`UUID_WIDTH-1:0] rsp_uuid;
+    wire [`NW_WIDTH-1:0] req_tag, rsp_tag;    
+
+    wire icache_req_fire = icache_req_valid && icache_req_ready;
+
+    wire [ISW_WIDTH-1:0] schedule_isw = wid_to_isw(schedule_if.data.wid);
+    
+    assign req_tag = schedule_if.data.wid;
+    
+    assign {rsp_uuid, rsp_tag} = icache_bus_if.rsp_data.tag;
+
+    wire [`XLEN-1:0] rsp_PC;
+    wire [THREAD_CNT-1:0] rsp_tmask;
+
+    // buffer to store the PC and thread mask until the response is received from the icache
+    localparam IBUF_SIZE_WIDTH = `CLOG2(`IBUF_SIZE+1);
+    wire icache_response_squashed_successfully;	
+    reg squashing_in_progress;
+    reg [IBUF_SIZE_WIDTH:0] sent_icache_requests;
+    wire [IBUF_SIZE_WIDTH:0] sent_icache_requests_n;
+
+    VX_fifo_queue #(
+        .DATAW  (`XLEN + THREAD_CNT),
+        .DEPTH  (`IBUF_SIZE),
+        .LUTRAM (1)
+    ) tag_store (
+        .clk   (clk),  
+        .reset (reset | (|branch_mispredict_flush)),
+        .push  (icache_bus_if.req_valid & icache_bus_if.req_ready),        
+        .pop   (icache_bus_if.rsp_valid & !squashing_in_progress), //only pop when response received which is not squashed
+        `UNUSED_PIN (empty),
+        `UNUSED_PIN (alm_empty),
+        `UNUSED_PIN (full),
+        `UNUSED_PIN (alm_full),
+        `UNUSED_PIN (size),
+        .data_in ({{icache_bus_if.req_data.addr,2'b0}, {(THREAD_CNT){1'b1}} }),
+        .data_out ({rsp_PC, rsp_tmask})
+    );
+
+    // to raise assertion errors for out of order responses
+    //
+    // Note: I suspect that the icache returns out of order. This would cause
+    // a problem when we are operating in 1Warp 1Thread config. We have seen
+    // icache do out of order responses for instructions of different warps 
+    // but I am yet to see a out of order response for instructions within
+    // a single warp. I believe 99% that the icache does not do out of order
+    // response/returns for instructions within a single warp. Cause if it did
+    // then the program order would get messed up and there is no
+    // mechanism in this pipeline that does reordering of the
+    // returned instructions. But I have not been able to confirm it so
+    // as a fail safe I have a put a tracker mechanism with assertion that
+    // will fail if there is a out of order response from icache within
+    // a single warp. It is PITA to debug if the program order gets messed so
+    // assertion is a better solution.
+    //
+    // TODO : remove the ooo_tracking fifo before synthesis/tapeout to save
+    // area/power
+
+    wire [(`UUID_WIDTH+`NW_WIDTH)-1:0] tmp;
+    VX_fifo_queue #(
+        .DATAW  (`UUID_WIDTH+`NW_WIDTH),
+        .DEPTH  (4*`IBUF_SIZE),
+        .LUTRAM (1)
+    ) ooo_tracking (
+        .clk   (clk),  
+        .reset (reset),
+        .push  (icache_bus_if.req_valid & icache_bus_if.req_ready),        
+        .pop   (icache_bus_if.rsp_valid),
+        `UNUSED_PIN (empty),
+        `UNUSED_PIN (alm_empty),
+        `UNUSED_PIN (full),
+        `UNUSED_PIN (alm_full),
+        `UNUSED_PIN (size),
+        .data_in (icache_bus_if.req_data.tag),
+        .data_out (tmp)
+    );
+	`RUNTIME_ASSERT(!(icache_bus_if.rsp_valid & (tmp != icache_bus_if.rsp_data.tag)), ("Possibly a OOO ICache Return???? Not sure. Please check icache responses") )
+
+
+    // Ensure that the ibuffer doesn't fill up.
+    // This resolves potential deadlock if ibuffer fills and the LSU stalls the execute stage due to pending dcache request.
+    // This issue is particularly prevalent when the icache and dcache is disabled and both requests share the same bus.
+    wire [`ISSUE_WIDTH-1:0] pending_ibuf_full;
+    for (genvar i = 0; i < `ISSUE_WIDTH; ++i) begin
+        VX_pending_size #( 
+            .SIZE (`IBUF_SIZE)
+        ) pending_reads (
+            .clk   (clk),
+            .reset (reset | (|branch_mispredict_flush) ),
+            .incr  (icache_req_fire && schedule_isw == i),
+            .decr  (fetch_if.ibuf_pop[i]),
+            .full  (pending_ibuf_full[i]),
+            `UNUSED_PIN (size),
+            `UNUSED_PIN (empty)
+        );
+    end
+
+    `RUNTIME_ASSERT((!schedule_if.valid || schedule_if.data.PC != 0), 
+        ("%t: *** invalid PC=0x%0h, wid=%0d, tmask=%b (#%0d)", $time, schedule_if.data.PC, schedule_if.data.wid, schedule_if.data.tmask, schedule_if.data.uuid))
+
+    // Icache Request
+    
+    wire ibuf_ready = ~pending_ibuf_full[schedule_isw];
+    assign icache_req_valid = schedule_if.valid && ibuf_ready;
+    assign icache_req_addr  = schedule_if.data.PC[`MEM_ADDR_WIDTH-1:2];
+    assign icache_req_tag   = {schedule_if.data.uuid, req_tag};
+    assign schedule_if.ready = icache_req_ready && ibuf_ready;
+
+    wire req_buf_out_valid, req_buf_out_ready;
+    assign req_buf_out_ready = icache_bus_if.req_ready & !squashing_in_progress;
+
+    VX_elastic_buffer #(
+        .DATAW   (ICACHE_ADDR_WIDTH + ICACHE_TAG_WIDTH),
+        .SIZE    (2),
+        .OUT_REG (1) // external bus should be registered
+    ) req_buf (
+        .clk       (clk),
+        .reset     (reset | (|branch_mispredict_flush)),
+        .valid_in  (icache_req_valid),
+        .ready_in  (icache_req_ready),
+        .data_in   ({icache_req_addr, icache_req_tag}),
+        .data_out  ({icache_bus_if.req_data.addr, icache_bus_if.req_data.tag}),
+        .valid_out (req_buf_out_valid),
+        .ready_out (req_buf_out_ready)
+    );
+
+    assign icache_bus_if.req_valid       = req_buf_out_valid & !squashing_in_progress;
+    assign icache_bus_if.req_data.rw     = 0;
+    assign icache_bus_if.req_data.byteen = 4'b1111;
+    assign icache_bus_if.req_data.data   = '0;    
+
+    // Icache Response
+
+    assign fetch_if.valid = icache_bus_if.rsp_valid & !squashing_in_progress;
+    assign fetch_if.data.tmask = rsp_tmask;
+    assign fetch_if.data.wid   = rsp_tag;
+    assign fetch_if.data.PC    = rsp_PC;
+    assign fetch_if.data.instr = icache_bus_if.rsp_data.data;
+    assign fetch_if.data.uuid  = rsp_uuid;
+    assign icache_bus_if.rsp_ready = fetch_if.ready;
+
+    // Squash Icache responses
+    //
+    // Below logic squashes icache responses. This is required when there has
+    // been a mispredict in the pipeline and we need to flush speculatively
+    // scheduled instructions. When we flush pipeline, it is possible that
+    // a fetch request was already sent to the icache which was not required. Since
+    // there is no way to stop a fetch once sent to Icache, we just squash the
+    // response once it comes back from Icache.
+		
+    assign icache_response_squashed_successfully = icache_bus_if.rsp_valid & squashing_in_progress;
+    assign sent_icache_requests_n = sent_icache_requests + IBUF_SIZE_WIDTH'(icache_bus_if.req_valid && icache_bus_if.req_ready) - IBUF_SIZE_WIDTH'(icache_bus_if.rsp_valid);
+	
+    always@ (posedge clk) begin
+        if (reset) begin
+            squashing_in_progress <= 0;
+        end
+        else begin
+            // If squashing in progress then continue till pending reads become zero
+            // else keep waiting for flush signal to start squashing
+            squashing_in_progress <= squashing_in_progress ? (sent_icache_requests != 0) : (|branch_mispredict_flush);
+        end
+    end
+
+    // track the number of icache requests in progress (i.e. the number of 
+    // instructions requested but not received)
+    always@(posedge clk) begin
+        if (reset)
+            sent_icache_requests <= 0;
+        else
+            sent_icache_requests <= sent_icache_requests_n;
+    end
+
+`ifdef DBG_SCOPE_FETCH
+    if (CORE_ID == 0) begin
+    `ifdef SCOPE
+        wire schedule_fire = schedule_if.valid && schedule_if.ready;
+        wire icache_rsp_fire = icache_bus_if.rsp_valid && icache_bus_if.rsp_ready;
+        VX_scope_tap #(
+            .SCOPE_ID (1),
+            .TRIGGERW (4),
+            .PROBEW   (3*`UUID_WIDTH + 108)
+        ) scope_tap (
+            .clk(clk),
+            .reset(scope_reset),
+            .start(1'b0),
+            .stop(1'b0),
+            .triggers({
+                reset,
+                schedule_fire,
+                icache_req_fire,
+                icache_rsp_fire
+            }),
+            .probes({
+                schedule_if.data.uuid, schedule_if.data.wid, schedule_if.data.tmask, schedule_if.data.PC,
+                icache_bus_if.req_data.tag, icache_bus_if.req_data.byteen, icache_bus_if.req_data.addr,
+                icache_bus_if.rsp_data.data, icache_bus_if.rsp_data.tag
+            }),
+            .bus_in(scope_bus_in),
+            .bus_out(scope_bus_out)
+        );
+    `endif
+    `ifdef CHIPSCOPE
+        ila_fetch ila_fetch_inst (
+            .clk    (clk),
+            .probe0 ({reset, schedule_if.data.uuid, schedule_if.data.wid, schedule_if.data.tmask, schedule_if.data.PC, schedule_if.ready, schedule_if.valid}),        
+            .probe1 ({icache_bus_if.req_data.tag, icache_bus_if.req_data.byteen, icache_bus_if.req_data.addr, icache_bus_if.req_ready, icache_bus_if.req_valid}),
+            .probe2 ({icache_bus_if.rsp_data.data, icache_bus_if.rsp_data.tag, icache_bus_if.rsp_ready, icache_bus_if.rsp_valid})
+        );
+    `endif
+    end
+`else
+    `SCOPE_IO_UNUSED()
+`endif
+
+`ifdef DBG_TRACE_CORE_ICACHE
+    wire schedule_fire = schedule_if.valid && schedule_if.ready;
+    wire fetch_fire = fetch_if.valid && fetch_if.ready;
+    always @(posedge clk) begin
+        if (schedule_fire) begin
+            `TRACE(1, ("%d: I$%0d req: wid=%0d, PC=0x%0h, tmask=%b (#%0d)\n", $time, CORE_ID, schedule_if.data.wid, schedule_if.data.PC, schedule_if.data.tmask, schedule_if.data.uuid));
+        end
+        if (fetch_fire) begin
+            `TRACE(1, ("%d: I$%0d rsp: wid=%0d, PC=0x%0h, tmask=%b, instr=0x%0h (#%0d)\n", $time, CORE_ID, fetch_if.data.wid, fetch_if.data.PC, fetch_if.data.tmask, fetch_if.data.instr, fetch_if.data.uuid));
+        end
+    end
+`endif
+
+endmodule
---
+++ hw/rtl/core/scalar/VX_sfu_unit_scalar.sv
@@ -0,0 +1,432 @@
+// Copyright © 2019-2023
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+`include "VX_define.vh"
+
+module VX_sfu_unit import VX_gpu_pkg::*; #(
+    parameter CORE_ID = 0,
+    parameter THREAD_CNT = `NUM_THREADS
+) (    
+    input wire              clk,
+    input wire              reset,
+
+`ifdef PERF_ENABLE
+    VX_mem_perf_if.slave    mem_perf_if,
+    VX_pipeline_perf_if.slave pipeline_perf_if,
+`endif
+
+    input base_dcrs_t       base_dcrs,
+
+    // Inputs
+    VX_dispatch_if.slave    dispatch_if [`ISSUE_WIDTH],
+    
+`ifdef EXT_F_ENABLE
+    VX_fpu_to_csr_if.slave  fpu_to_csr_if [`NUM_FPU_BLOCKS],
+`endif
+
+`ifdef EXT_TEX_ENABLE
+    VX_tex_bus_if.master    tex_bus_if,
+`ifdef PERF_ENABLE
+    VX_tex_perf_if.slave    perf_tex_if,
+    VX_cache_perf_if.slave  perf_tcache_if,
+`endif
+`endif
+
+`ifdef EXT_RASTER_ENABLE
+    VX_raster_bus_if.slave  raster_bus_if,
+`ifdef PERF_ENABLE
+    VX_raster_perf_if.slave perf_raster_if,
+    VX_cache_perf_if.slave  perf_rcache_if,
+`endif
+`endif
+
+`ifdef EXT_ROP_ENABLE
+    VX_rop_bus_if.master    rop_bus_if,
+`ifdef PERF_ENABLE
+    VX_rop_perf_if.slave    perf_rop_if,
+    VX_cache_perf_if.slave  perf_ocache_if,
+`endif
+`endif
+
+    // Outputs
+    VX_commit_if.master     commit_if [`ISSUE_WIDTH],
+    VX_commit_csr_if.slave  commit_csr_if,
+    VX_sched_csr_if.slave   sched_csr_if,
+    VX_warp_ctl_if.master   warp_ctl_if    
+);
+    `UNUSED_PARAM (CORE_ID)
+    localparam BLOCK_SIZE   = 1;
+    localparam NUM_LANES    = `MIN(`NUM_SFU_LANES,THREAD_CNT);
+    localparam PID_BITS     = `CLOG2(THREAD_CNT / NUM_LANES);
+    localparam PID_WIDTH    = `UP(PID_BITS);
+
+    localparam RSP_ARB_DATAW = `UUID_WIDTH + `NW_WIDTH + NUM_LANES + (NUM_LANES * `XLEN) + `NR_BITS + 1 + `XLEN + PID_WIDTH + 1 + 1 + 1;
+    localparam RSP_ARB_SIZE = 1 + 1 + `EXT_TEX_ENABLED + `EXT_RASTER_ENABLED + `EXT_ROP_ENABLED;
+    localparam RSP_ARB_IDX_WCTL = 0;
+    localparam RSP_ARB_IDX_CSR = 1;
+    localparam RSP_ARB_IDX_RASTER = RSP_ARB_IDX_CSR + 1;
+    localparam RSP_ARB_IDX_ROP = RSP_ARB_IDX_RASTER + `EXT_RASTER_ENABLED;    
+    localparam RSP_ARB_IDX_TEX = RSP_ARB_IDX_ROP + `EXT_ROP_ENABLED;
+    `UNUSED_PARAM (RSP_ARB_IDX_RASTER)
+    `UNUSED_PARAM (RSP_ARB_IDX_ROP)
+    `UNUSED_PARAM (RSP_ARB_IDX_TEX)
+
+    VX_execute_if #(
+        .THREAD_CNT(THREAD_CNT),
+        .NUM_LANES (NUM_LANES)
+    ) execute_if[BLOCK_SIZE]();
+
+    `RESET_RELAY (dispatch_reset, reset);
+
+    VX_dispatch_unit #(
+        .BLOCK_SIZE (BLOCK_SIZE),
+        .NUM_LANES  (NUM_LANES),
+        .OUT_REG    (1),
+        .THREAD_CNT(THREAD_CNT)
+    ) dispatch_unit (
+        .clk        (clk),
+        .reset      (dispatch_reset),
+        .dispatch_if(dispatch_if),
+        .execute_if (execute_if)
+    );
+
+    wire [RSP_ARB_SIZE-1:0] rsp_arb_valid_in;
+    wire [RSP_ARB_SIZE-1:0] rsp_arb_ready_in;
+    wire [RSP_ARB_SIZE-1:0][RSP_ARB_DATAW-1:0] rsp_arb_data_in;
+    
+`ifdef PERF_ENABLE
+    VX_sfu_perf_if sfu_perf_if();
+`endif
+
+`ifdef EXT_TEX_ENABLE
+    VX_sfu_csr_if  #(.THREAD_CNT(THREAD_CNT)) tex_csr_if();
+`endif
+
+`ifdef EXT_RASTER_ENABLE
+    VX_sfu_csr_if  #(.THREAD_CNT(THREAD_CNT)) raster_csr_if();
+`endif
+
+`ifdef EXT_ROP_ENABLE
+    VX_sfu_csr_if #(.THREAD_CNT(THREAD_CNT)) rop_csr_if();
+`endif
+
+    // Warp control block    
+    VX_execute_if #(
+        .THREAD_CNT(THREAD_CNT),
+        .NUM_LANES (NUM_LANES)
+    ) wctl_execute_if();
+    VX_commit_if#(
+        .THREAD_CNT(THREAD_CNT),
+        .NUM_LANES (NUM_LANES)
+    ) wctl_commit_if();
+    
+    assign wctl_execute_if.valid = execute_if[0].valid && `INST_SFU_IS_WCTL(execute_if[0].data.op_type);
+    assign wctl_execute_if.data = execute_if[0].data;
+
+    `RESET_RELAY (wctl_reset, reset);
+    
+    VX_wctl_unit #(
+        .CORE_ID   (CORE_ID),
+        .THREAD_CNT(THREAD_CNT),
+        .NUM_LANES (NUM_LANES)
+    ) wctl_unit (
+        .clk        (clk),
+        .reset      (wctl_reset),
+        .execute_if (wctl_execute_if), 
+        .warp_ctl_if(warp_ctl_if), 
+        .commit_if  (wctl_commit_if)
+    );
+
+    assign rsp_arb_valid_in[RSP_ARB_IDX_WCTL] = wctl_commit_if.valid;
+    assign rsp_arb_data_in[RSP_ARB_IDX_WCTL] = wctl_commit_if.data;
+    assign wctl_commit_if.ready = rsp_arb_ready_in[RSP_ARB_IDX_WCTL];
+
+    // CSR unit
+    VX_execute_if #(
+        .THREAD_CNT(THREAD_CNT),
+        .NUM_LANES (NUM_LANES)
+    ) csr_execute_if();
+    VX_commit_if #(
+        .THREAD_CNT(THREAD_CNT),
+        .NUM_LANES (NUM_LANES)
+    ) csr_commit_if();
+
+    assign csr_execute_if.valid = execute_if[0].valid && `INST_SFU_IS_CSR(execute_if[0].data.op_type);
+    assign csr_execute_if.data = execute_if[0].data;
+
+    `RESET_RELAY (csr_reset, reset);
+
+    VX_csr_unit #(
+        .CORE_ID   (CORE_ID),
+        .NUM_LANES (NUM_LANES),
+        .THREAD_CNT(THREAD_CNT)
+    ) csr_unit (
+        .clk            (clk),
+        .reset          (csr_reset),
+
+        .base_dcrs      (base_dcrs),
+        .execute_if     (csr_execute_if),
+    
+    `ifdef PERF_ENABLE
+        .mem_perf_if    (mem_perf_if),
+        .pipeline_perf_if(pipeline_perf_if),
+        .sfu_perf_if    (sfu_perf_if),
+    `endif
+   
+    `ifdef EXT_F_ENABLE  
+        .fpu_to_csr_if  (fpu_to_csr_if),
+    `endif        
+    
+    `ifdef EXT_TEX_ENABLE        
+        .tex_csr_if     (tex_csr_if),
+    `ifdef PERF_ENABLE
+        .perf_tex_if    (perf_tex_if),
+        .perf_tcache_if (perf_tcache_if),
+    `endif
+    `endif
+    
+    `ifdef EXT_RASTER_ENABLE        
+        .raster_csr_if  (raster_csr_if),
+    `ifdef PERF_ENABLE
+        .perf_raster_if (perf_raster_if),
+        .perf_rcache_if (perf_rcache_if),
+    `endif
+    `endif
+
+    `ifdef EXT_ROP_ENABLE        
+        .rop_csr_if     (rop_csr_if),
+    `ifdef PERF_ENABLE
+        .perf_rop_if    (perf_rop_if),
+        .perf_ocache_if (perf_ocache_if),
+    `endif
+    `endif
+
+        .sched_csr_if   (sched_csr_if),
+        .commit_csr_if  (commit_csr_if),
+        .commit_if      (csr_commit_if)
+    );    
+
+    assign rsp_arb_valid_in[RSP_ARB_IDX_CSR] = csr_commit_if.valid;
+    assign rsp_arb_data_in[RSP_ARB_IDX_CSR] = csr_commit_if.data;
+    assign csr_commit_if.ready = rsp_arb_ready_in[RSP_ARB_IDX_CSR];
+    
+`ifdef EXT_TEX_ENABLE
+
+    VX_execute_if #(
+        .THREAD_CNT(THREAD_CNT),
+        .NUM_LANES (NUM_LANES)
+    ) tex_execute_if();
+    VX_commit_if #(
+        .THREAD_CNT(THREAD_CNT),
+        .NUM_LANES (NUM_LANES)
+    ) tex_commit_if();
+
+    assign tex_execute_if.valid = execute_if[0].valid && (execute_if[0].data.op_type == `INST_SFU_TEX);
+    assign tex_execute_if.data = execute_if[0].data;
+
+    `RESET_RELAY (tex_reset, reset);
+
+    VX_tex_agent #(
+        .CORE_ID   (CORE_ID),
+        .NUM_LANES (NUM_LANES)
+    ) tex_agent (
+        .clk        (clk),
+        .reset      (tex_reset),
+        .execute_if (tex_execute_if),
+        .tex_csr_if (tex_csr_if),
+        .tex_bus_if (tex_bus_if),
+        .commit_if  (tex_commit_if)        
+    );     
+
+    assign rsp_arb_valid_in[RSP_ARB_IDX_TEX] = tex_commit_if.valid;
+    assign rsp_arb_data_in[RSP_ARB_IDX_TEX] = tex_commit_if.data;
+    assign tex_commit_if.ready = rsp_arb_ready_in[RSP_ARB_IDX_TEX];
+
+`endif
+
+`ifdef EXT_RASTER_ENABLE
+    
+    VX_execute_if #(
+        .THREAD_CNT(THREAD_CNT),
+        .NUM_LANES (NUM_LANES)
+    ) raster_execute_if();
+    VX_commit_if #(
+        .THREAD_CNT(THREAD_CNT),
+        .NUM_LANES (NUM_LANES)
+    ) raster_commit_if();
+
+    assign raster_execute_if.valid = execute_if[0].valid && (execute_if[0].data.op_type == `INST_SFU_RASTER);
+    assign raster_execute_if.data = execute_if[0].data;
+
+    `RESET_RELAY (raster_reset, reset);
+
+    VX_raster_agent #(
+        .CORE_ID   (CORE_ID),
+        .NUM_LANES (NUM_LANES),
+        .THREAD_CNT(THREAD_CNT)
+    ) raster_agent (
+        .clk        (clk),
+        .reset      (raster_reset),
+        .execute_if (raster_execute_if),
+        .raster_csr_if(raster_csr_if),
+        .raster_bus_if(raster_bus_if), 
+        .commit_if  (raster_commit_if)       
+    );
+
+    assign rsp_arb_valid_in[RSP_ARB_IDX_RASTER] = raster_commit_if.valid;
+    assign rsp_arb_data_in[RSP_ARB_IDX_RASTER] = raster_commit_if.data;
+    assign raster_commit_if.ready = rsp_arb_ready_in[RSP_ARB_IDX_RASTER];
+
+`endif
+
+`ifdef EXT_ROP_ENABLE
+    
+    VX_execute_if #(
+        .THREAD_CNT(THREAD_CNT),
+        .NUM_LANES (NUM_LANES)
+    ) rop_execute_if();
+    VX_commit_if #(
+        .THREAD_CNT(THREAD_CNT),
+        .NUM_LANES (NUM_LANES)
+    ) rop_commit_if();
+
+    assign rop_execute_if.valid = execute_if[0].valid && (execute_if[0].data.op_type == `INST_SFU_ROP);
+    assign rop_execute_if.data = execute_if[0].data;
+
+    `RESET_RELAY (rop_reset, reset);
+            
+    VX_rop_agent #(
+        .CORE_ID   (CORE_ID),
+        .NUM_LANES (NUM_LANES),
+        .THREAD_CNT(THREAD_CNT)
+    ) rop_agent (
+        .clk        (clk),
+        .reset      (rop_reset),
+        .execute_if (rop_execute_if),
+        .rop_csr_if (rop_csr_if),
+        .rop_bus_if (rop_bus_if),
+        .commit_if  (rop_commit_if)
+    );
+
+    assign rsp_arb_valid_in[RSP_ARB_IDX_ROP] = rop_commit_if.valid;
+    assign rsp_arb_data_in[RSP_ARB_IDX_ROP] = rop_commit_if.data;
+    assign rop_commit_if.ready = rsp_arb_ready_in[RSP_ARB_IDX_ROP];
+
+`endif
+
+    // can accept new request?
+
+    reg sfu_req_ready;
+    always @(*) begin
+        case (execute_if[0].data.op_type)
+         `INST_SFU_CSRRW,
+         `INST_SFU_CSRRS,
+         `INST_SFU_CSRRC: sfu_req_ready = csr_execute_if.ready;
+    `ifdef EXT_TEX_ENABLE
+        `INST_SFU_TEX: sfu_req_ready = tex_execute_if.ready;
+    `endif
+    `ifdef EXT_RASTER_ENABLE
+        `INST_SFU_RASTER: sfu_req_ready = raster_execute_if.ready;
+    `endif
+    `ifdef EXT_ROP_ENABLE
+        `INST_SFU_ROP: sfu_req_ready = rop_execute_if.ready;
+    `endif
+        default: sfu_req_ready = wctl_execute_if.ready;
+        endcase
+    end   
+    assign execute_if[0].ready = sfu_req_ready;
+
+    // response arbitration
+    
+    `RESET_RELAY (commit_reset, reset);
+
+    VX_commit_if #(
+        .THREAD_CNT(THREAD_CNT),
+        .NUM_LANES (NUM_LANES)
+    ) arb_commit_if[BLOCK_SIZE]();
+
+    VX_stream_arb #(
+        .NUM_INPUTS (RSP_ARB_SIZE),
+        .DATAW      (RSP_ARB_DATAW),
+        .ARBITER    ("R"),
+        .OUT_REG    (1)
+    ) rsp_arb (
+        .clk       (clk),
+        .reset     (commit_reset), 
+        .valid_in  (rsp_arb_valid_in),
+        .ready_in  (rsp_arb_ready_in),
+        .data_in   (rsp_arb_data_in),
+        .data_out  (arb_commit_if[0].data),
+        .valid_out (arb_commit_if[0].valid),
+        .ready_out (arb_commit_if[0].ready),
+        `UNUSED_PIN (sel_out)
+    );
+
+    VX_gather_unit #(
+        .BLOCK_SIZE (BLOCK_SIZE),
+        .NUM_LANES  (NUM_LANES),
+        .OUT_REG    (3),
+        .THREAD_CNT(THREAD_CNT)
+    ) gather_unit (
+        .clk           (clk),
+        .reset         (commit_reset),
+        .commit_in_if  (arb_commit_if),
+        .commit_out_if (commit_if)
+    );
+
+`ifdef PERF_ENABLE
+`ifdef EXT_TEX_ENABLE
+    reg [`PERF_CTR_BITS-1:0] perf_tex_stalls;
+    always @(posedge clk) begin
+        if (reset) begin
+            perf_tex_stalls <= '0;
+        end else begin
+            perf_tex_stalls <= perf_tex_stalls + `PERF_CTR_BITS'(tex_execute_if.valid && ~tex_execute_if.ready);
+        end
+    end
+    assign sfu_perf_if.tex_stalls = perf_tex_stalls;
+`endif
+`ifdef EXT_RASTER_ENABLE
+    reg [`PERF_CTR_BITS-1:0] perf_raster_stalls;
+    always @(posedge clk) begin
+        if (reset) begin
+            perf_raster_stalls <= '0;
+        end else begin
+            perf_raster_stalls <= perf_raster_stalls + `PERF_CTR_BITS'(raster_execute_if.valid && ~raster_execute_if.ready);
+        end
+    end
+    assign sfu_perf_if.raster_stalls = perf_raster_stalls;
+`endif
+`ifdef EXT_ROP_ENABLE
+    reg [`PERF_CTR_BITS-1:0] perf_rop_stalls;
+    always @(posedge clk) begin
+        if (reset) begin
+            perf_rop_stalls <= '0;
+        end else begin
+            perf_rop_stalls <= perf_rop_stalls + `PERF_CTR_BITS'(rop_execute_if.valid && ~rop_execute_if.ready);
+        end
+    end
+    assign sfu_perf_if.rop_stalls = perf_rop_stalls;
+`endif
+    reg [`PERF_CTR_BITS-1:0] perf_wctl_stalls;
+    always @(posedge clk) begin
+        if (reset) begin
+            perf_wctl_stalls <= '0;
+        end else begin
+            perf_wctl_stalls <= perf_wctl_stalls + `PERF_CTR_BITS'(wctl_execute_if.valid && ~wctl_execute_if.ready);
+        end
+    end
+    assign sfu_perf_if.wctl_stalls = perf_wctl_stalls;
+`endif
+
+endmodule
